1d11eb20e177e6591f6175d7e4bf3200ac39a52d
Amy Y <ctrlalternativedelete@gmail.com>
2024-04-23 23:23:29 +1000

    wip

--- src/main/java/StoreController.java
+++ src/main/java/StoreController.java
@@ -1,1 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class StoreController {
+    @FXML private Button sellBtn;
+    @FXML private Text stockTxt;
+    @FXML private Text priceTxt;
+    @FXML private TextField amountTf;
+    @FXML private Text cashTxt;
+}
